78254b20f88f9279e325fdc7e383476fa3cb14bd
Dadadam <bogdanov.andrey.code@gmail.com>
2024-01-14 00:06:32 +0200

    Fix service

--- src/Factory/ServiceFactory.php
+++ src/Factory/ServiceFactory.php
@@ -9,14 +9,21 @@ class ServiceFactory
 {
     private static $services = [];
 
+    private static $classMap = [
+        'ScriptController' => ScriptController::class,
+        'MenuController' => MenuController::class,
+    ];
+
     public static function getService(string $name): object
     {
+        if (!isset(self::$classMap[$name])) {
+            throw new \InvalidArgumentException("No service found for $name");
+        }
+
+        $className = self::$classMap[$name];
+
         if (!isset(self::$services[$name])) {
-            self::$services[$name] = match ($name) {
-                ScriptController::class => new ScriptController(),
-                MenuController::class => new MenuController(),
-                default => throw new \InvalidArgumentException("No service found for $name")
-            };
+            self::$services[$name] = new $className();
         }
 
         return self::$services[$name];
