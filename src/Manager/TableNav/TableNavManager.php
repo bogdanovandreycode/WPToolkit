441a9f3ac4840ff5c6d841bd745115e7b942c443
Dadadam <bogdanov.andrey.code@gmail.com>
2025-04-06 00:18:06 +0300

    Add strategy for table nav elements

--- src/Manager/TableNav/TableNavManager.php
+++ src/Manager/TableNav/TableNavManager.php
@@ -2,54 +2,24 @@
 
 namespace WpToolKit\Manager\TableNav;
 
-use WP_Query;
 use WP_Screen;
+use WP_Query;
+use WP_User_Query;
 use WpToolKit\Interface\TableNav\TableNavElementInterface;
+use WpToolKit\Interface\TableNav\TableNavStrategyInterface;
 
 class TableNavManager
 {
-    private string $screenId;
-    private ?string $postType;
     private array $elements = [];
 
-    /**
-     * @param string $screenId Screen ID, например "edit-post", "edit-tutor_profiles", "users"
-     * @param string|null $postType Опциональный post_type (например 'tutor_profiles')
-     */
-    public function __construct(string $screenId, ?string $postType = null)
-    {
-        $this->screenId = $screenId;
-        $this->postType = $postType;
-
-        $hooks = $this->getHookNames();
-
-        add_action($hooks['render'], [$this, 'render']);
-        add_filter($hooks['apply'], [$this, 'apply']);
+    public function __construct(
+        private string $screenId,
+        private ?string $postType,
+        private TableNavStrategyInterface $strategy
+    ) {
+        add_action($strategy->getRenderHook($screenId), [$this, 'render']);
+        add_filter($strategy->getApplyHook($screenId), [$this, 'apply']);
     }
-
-    private function getHookNames(): array
-    {
-        switch ($this->screenId) {
-            case 'users':
-                return [
-                    'render' => 'restrict_manage_users',
-                    'apply'  => 'pre_get_users',
-                ];
-
-            case 'upload':
-                return [
-                    'render' => 'restrict_manage_posts',
-                    'apply'  => 'parse_query',
-                ];
-
-            default:
-                return [
-                    'render' => 'restrict_manage_posts',
-                    'apply'  => 'parse_query',
-                ];
-        }
-    }
-
 
     public function addElement(TableNavElementInterface $element): void
     {
@@ -58,13 +28,7 @@ class TableNavManager
 
     public function render(): void
     {
-        $screen = get_current_screen();
-
-        if (
-            !($screen instanceof WP_Screen) ||
-            $screen->id !== $this->screenId ||
-            ($this->postType && $screen->post_type !== $this->postType)
-        ) {
+        if (!$this->strategy->isApplicable($this->screenId, $this->postType)) {
             return;
         }
 
@@ -82,12 +46,7 @@ class TableNavManager
             return;
         }
 
-        $screen = get_current_screen();
-        if (
-            !($screen instanceof WP_Screen) ||
-            $screen->id !== $this->screenId ||
-            ($this->postType && $screen->post_type !== $this->postType)
-        ) {
+        if (!$this->strategy->isApplicable($this->screenId, $this->postType)) {
             return;
         }
 
